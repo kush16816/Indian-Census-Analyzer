d45e3826da3d8754d75bf445c37a932dff5ac0fa
kush16816 <kushagravi13@gmail.com>
2020-10-21 07:48:28 +0530

    Added test case for incorrect file

--- src/test/java/com/capgemini/indiancensusanalyzer/AppTest.java
+++ src/test/java/com/capgemini/indiancensusanalyzer/AppTest.java
@@ -3,8 +3,12 @@ package com.capgemini.indiancensusanalyzer;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import org.junit.Test;
+
+import com.capgemini.indiancensusanalyzer.exception.StateCensusException;
+import com.capgemini.indiancensusanalyzer.exception.StateCensusException.Type;
 
 public class AppTest 
 {
@@ -13,5 +17,17 @@ public class AppTest
 		int result = StateCensusAnalyzer
 				.getCount(StateCensusAnalyzer.loadCSVFile(StateCensusAnalyzer.indianCensusData));
 		assertEquals(29, result);
+	}
+	
+	@Test
+	public void testForIncorrectFile() {
+		try {
+		if(Paths.get("src/main/resources/IndiaStateCensusData.csv")!=StateCensusAnalyzer.indianCensusDataFalseCount) {
+			StateCensusException stateCensusException = new StateCensusException(Type.FileNotFound, "File is incorrect");
+			throw stateCensusException;
+		}}
+		catch (StateCensusException stateCensusException) {
+			assertEquals("File is incorrect", stateCensusException.message);
+		}
 	}
 }
