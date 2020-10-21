99a7ad7608632d3d10741ee564afc006091ac81d
kush16816 <kushagravi13@gmail.com>
2020-10-21 07:17:10 +0530

    Added test case for number of records

--- src/test/java/com/capgemini/indiancensusanalyzer/AppTest.java
+++ src/test/java/com/capgemini/indiancensusanalyzer/AppTest.java
@@ -1,20 +1,17 @@
 package com.capgemini.indiancensusanalyzer;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void testForCorrectNumberOfRecords() throws IOException {
+		int result = StateCensusAnalyzer
+				.getCount(StateCensusAnalyzer.loadCSVFile(StateCensusAnalyzer.indianCensusData));
+		assertEquals(29, result);
+	}
 }
